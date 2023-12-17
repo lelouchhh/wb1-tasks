5f524ef80574614f8db595304f5a3fa16b8f007c
Grigoriy Panin <llchh@yahoo.com>
2023-12-17 16:40:29 +0700

    10

--- 10/main.go
+++ 10/main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
-func main() {
-	n := 0
-	if true {
-		n := 1
-		n++
+func groupTemperatures(temperatures []float64, step float64) map[int][]float64 {
+	groups := make(map[int][]float64)
+
+	roundDown := func(value, step float64) float64 {
+		return math.Floor(value/step) * step
 	}
-	fmt.Println(n)
+
+	sort.Float64s(temperatures)
+
+	for _, temp := range temperatures {
+		groupKey := int(roundDown(temp, step))
+		groups[groupKey] = append(groups[groupKey], temp)
+	}
+
+	return groups
+}
+
+func main() {
+	temperatures := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	step := 10.0
+
+	groups := groupTemperatures(temperatures, step)
+
+	for key, values := range groups {
+		fmt.Printf("%d: %v\n", key, values)
+	}
 }
